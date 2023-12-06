fb48c52ce3a2a41108cb07b724750f8ad27ede3b
Bookah0 <91531769+Bookah0@users.noreply.github.com>
2023-12-06 11:39:34 +0100

    popo

--- Day6.py
+++ Day6.py
@@ -1,25 +1,19 @@
-
-def got_first_place(speed, time_left, winning_distance):
-    return winning_distance < speed*(time_left)
-
-
 def parse_input1(race_map={}):
     lines = open('Input/Day6.txt', "r").readlines()
     times, distances = lines[0].split(), lines[1].split()
     return {'race'+str(i): {'distance': int(distances[i]), 'time': int(times[i])} for i in range(1, len(times))}
 
-
-def solve_input(race_map, total=1):
-    for m in race_map:
-        tot_time, distance = race_map[m]['time'], race_map[m]['distance']
-        total *= sum(1 for time_held in range(tot_time) if got_first_place(time_held, tot_time-time_held, distance))
-    print(total)
-
-
 def parse_input2():
     lines = open('Input/Day6.txt', "r").readlines()
     time_line, distance_line = lines[0][9:].replace(' ', ''), lines[1][9:].replace(' ', '')
     return { 'Race1': {'distance': int(distance_line), 'time': int(time_line)}}
+
+
+def solve_input(race_map, total=1):
+    for m in race_map:
+        tot_time, distance = race_map[m]['time'], race_map[m]['distance']
+        total *= sum(1 for time_held in range(tot_time) if distance < time_held * (tot_time-time_held))
+    print(total)
 
 
 if __name__ == '__main__':
