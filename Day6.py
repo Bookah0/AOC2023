bd07b94b031cf0cd53b0aac1263597a50c8efa99
Bookah0 <91531769+Bookah0@users.noreply.github.com>
2023-12-06 11:37:48 +0100

    popo

--- Day6.py
+++ Day6.py
@@ -1,26 +1,18 @@
 
-def get_travel_time(speed, time_left, winning_distance):
-    return winning_distance+1 <= speed*(time_left)
+def got_first_place(speed, time_left, winning_distance):
+    return winning_distance < speed*(time_left)
 
 
-def parse_input1():
-    race_map = {}
+def parse_input1(race_map={}):
     lines = open('Input/Day6.txt', "r").readlines()
-    time_line, distance_line = lines[0].split(), lines[1].split()
-    for i in range(1, len(time_line)):
-        race_map['race'+str(i)] = {'distance': int(distance_line[i]), 'time': int(time_line[i])}
-    return race_map
+    times, distances = lines[0].split(), lines[1].split()
+    return {'race'+str(i): {'distance': int(distances[i]), 'time': int(times[i])} for i in range(1, len(times))}
 
 
-def solve_input(race_map):
-    total = 1
+def solve_input(race_map, total=1):
     for m in race_map:
-        n_record_beats = 0
-        time, distance = race_map[m]['time'], race_map[m]['distance']
-        for time_held in range(time):
-            if get_travel_time(time_held, time-time_held, distance):
-                n_record_beats += 1
-        total *= n_record_beats
+        tot_time, distance = race_map[m]['time'], race_map[m]['distance']
+        total *= sum(1 for time_held in range(tot_time) if got_first_place(time_held, tot_time-time_held, distance))
     print(total)
 
 
@@ -34,5 +26,5 @@ if __name__ == '__main__':
     race_map_1 = parse_input1()
     race_map_2 = parse_input2()
 
-    # solve_input1(race_map_1)
-    solve_input2(race_map_2)
+    # solve_input(race_map_1)
+    solve_input(race_map_2)
