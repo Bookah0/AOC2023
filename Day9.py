913c17b91863bf05d8a5157dc9fb0cff69e013a5
Bookah0 <91531769+Bookah0@users.noreply.github.com>
2023-12-09 23:02:59 +0100

    popo

--- Day9.py
+++ Day9.py
@@ -1,42 +1,25 @@
-def parse_input():
-    return [[int(n) for n in line.strip().split()] for line in open('Input/Day9.txt', "r").readlines()]
 
 
 def solve_input1():
-    sequences = parse_input()
-    total = 0
-    for history in sequences:
-        cur_list, polate_lists = history, []
-        while cur_list:
-            polate_list = [cur_list[i+1] - cur_list[i] for i in range(len(cur_list) - 1)]
-            polate_lists.append(polate_list)
-            cur_list = polate_list
-            if polate_list[0] == 0 and len(set(polate_list)) == 1:
-                total += history[-1] + sum([l[-1] for l in polate_lists])
-                cur_list = []
-    return total
+    histories = [[[int(n) for n in line.strip().split()]] for line in open('Input/Day9.txt', "r").readlines()]
 
-def solve_input2():
-    sequences = parse_input()
-    total = 0
-    for history in sequences:
-        cur_list, polate_lists = history, []
-        while cur_list:
-            polate_list = [cur_list[i+1] - cur_list[i] for i in range(len(cur_list) - 1)]
-            polate_lists.append(polate_list)
-            cur_list = polate_list
-            print(cur_list)
-            if len(cur_list) == 0:
-                print(history)
+    for history_list in histories:
+        cur_list = history_list[0]
+
+        while True:
+            cur_list = [cur_list[i+1] - cur_list[i] for i in range(len(cur_list) - 1)]
+            history_list.append(cur_list)
+
             if cur_list[0] == 0 and len(set(cur_list)) == 1:
-                polate_lists[-1].insert(0, 0)
-                for i in range(len(polate_lists) - 1, -1, -1):
-                    if i > 0:
-                        polate_lists[i-1].insert(0, polate_lists[i-1][0]-polate_lists[i][0])
-                total += history[0] - polate_lists[0][0]
-                cur_list = []
-    return total
+                history_list[-1].insert(0, 0)
+                for i in range(len(history_list) - 1, 0, -1):
+                    history_list[i-1].insert(0, history_list[i-1][0]-history_list[i][0])
+                break
+
+    before = sum([history_list[0][0] for history_list in histories])
+    after = sum([l[-1] for history_list in histories for l in history_list])
+    return before, after
 
 
 if __name__ == '__main__':
-    print(solve_input2())
+    print(solve_input1())
